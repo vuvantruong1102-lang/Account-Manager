25b066073f12d6511d54914f34e9f2bfbb8427d6
vuvantruong1102-lang <vuvantruong.1102@gmail.com>
2026-07-22 15:43:59 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Pipeline from './pages/Pipeline'
 import Interactions from './pages/Interactions'
 import Quotes from './pages/Quotes'
 import PaymentRequest from './pages/PaymentRequest'
+import Delivery from './pages/Delivery'
 import Sales from './pages/Sales'
 
 function Protected({ children }) {
@@ -29,6 +30,7 @@ export default function App() {
       <Route path="/sales" element={<Protected><Sales /></Protected>} />
       <Route path="/quotes" element={<Protected><Quotes /></Protected>} />
       <Route path="/payment-requests" element={<Protected><PaymentRequest /></Protected>} />
+      <Route path="/delivery" element={<Protected><Delivery /></Protected>} />
       <Route path="/interactions" element={<Protected><Interactions /></Protected>} />
       <Route path="/customers/b2b" element={<Protected><Customers segment="b2b" /></Protected>} />
       <Route path="/customers/retail" element={<Protected><Customers segment="retail" /></Protected>} />
